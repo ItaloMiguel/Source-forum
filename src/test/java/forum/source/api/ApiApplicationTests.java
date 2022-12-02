cb6f1807c209e5707bff44d1449e328cc1ba01fa
ItaloMiguel <italomiguelleite16@gmail.com>
2022-12-01 23:27:44 -0300

    first commit

---
+++ src/test/java/forum/source/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.source.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
